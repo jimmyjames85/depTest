4081e83ff8523f0422444978fa8f9e0a2c361b58
jimmyjames <james.tappe@sendgrid.com>
2019-10-22 15:18:15 -0700

    internal folder should not cause dep collision

--- cmd/main.go
+++ cmd/main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 
 	depone "github.com/jimmyjames85/depOne"
+	deptwo "github.com/jimmyjames85/depTwo/pkg"
 )
 
 func main() {
 	fmt.Printf("calling depone.One(): %s\n", depone.One())
 	fmt.Printf("calling depone.OneInt(): %d\n", depone.OneInt())
+	fmt.Printf("calling deptwo.Two(): %s\n", deptwo.Two())
 }
